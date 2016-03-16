785ff48609654cbf8072661fba14894a4251807a
Saffron <saffronbeatson@gmail.com>
2016-03-16 16:06:52 +0200

    Second test passing

--- CODEX/Eating-Out/eating-out.js
+++ CODEX/Eating-Out/eating-out.js
@@ -1,5 +1,10 @@
 exports.numbersArray = function(firstText){
-var firstText = "Hi Xola thanks for joining us last night. Your team ate 8 burgers, drank 17 beers and 11 fanta's"
+
 var nArray = firstText.match(/\d+/g);
 return nArray;
+};
+
+exports.priceArray = function(secondText) {
+var pArray = secondText.match(/\d+/g);
+return pArray;
 };
